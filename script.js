fb6c93dfcf57f2389ee64e429bf37b84cf491563
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2023-08-12 21:28:46 -0300

    Função principal finalizada

--- script.js
+++ script.js
@@ -26,3 +26,50 @@ function AreaDoCirculo() {
   const raio = parseFloat(prompt("Digite o valor do raio do circulo"));
   return 3.14 * raio * raio;
 }
+function Menu() {
+  return prompt(
+    " Calculadora Geométrica\n" +
+      "1. Calcular área de triângulo\n" +
+      "2. Calcular área de retângulo\n" +
+      "3. Calcular área de quadrado\n" +
+      "4. Calcular área de trapézio\n" +
+      "5. Calcular área de círculo\n" +
+      "6. Sair\n"
+  );
+}
+function Main() {
+  let opçao = "";
+
+  do {
+    opçao = Menu();
+    let resultado;
+
+    switch (opçao) {
+      case "1":
+        resultado = CalcularAreaDoTriangulo();
+        break;
+      case "2":
+        resultado = CalcularAreaDoRetangulo();
+        break;
+      case "3":
+        resultado = AreaDoQuadrado();
+        break;
+      case "4":
+        resultado = CalcularAreaDoTrapezio();
+        break;
+      case "5":
+        resultado = AreaDoCirculo();
+        break;
+      case "6":
+        alert("Saindo");
+        break;
+      default:
+        alert("Opção invalida!");
+        break;
+    }
+    if (resultado) {
+      alert("Resultado: " + resultado);
+    }
+  } while (opçao !== "6");
+}
+Main();
